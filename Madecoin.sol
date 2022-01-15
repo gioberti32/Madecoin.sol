72fd78fe65a466b55b4219e3d9e668f3b1dde6bf
Madecoin <97768614+Madecoin1@users.noreply.github.com>
2022-01-15 13:07:20 +0100

    Update Madecoin.sol

--- Madecoin.sol
+++ Madecoin.sol
@@ -8,15 +8,7 @@
 
 /**
   
-   #BEE
-   
-   #LIQ+#RFI+#SHIB+#DOGE = #BEE
 
-   #SAFEMOON features:
-   3% fee auto add to the liquidity pool to locked forever when selling
-   2% fee auto distribute to all holders
-   I created a black hole so #Bee token will deflate itself in supply with every transaction
-   50% Supply is burned at start.
    
 
  */
